c01f5431046202aebe1813e59bb42b8537789fab
Felipe <villajaramillofelipe4@gmail.com>
2025-04-29 13:28:54 -0500

    Clase de polinomio de lagrange y ejercicios. TODO: Arreglar polinomial simple

---
+++ Analisis_numerico/polinomio_de_lagrange/lagrange.py
@@ -0,0 +1,16 @@
+import sympy as sp
+import numpy as np
+x = sp.symbols('x')
+
+def lagrange(x_data, y_data):
+    
+    P = 0 # Acumulador del polinomio
+    n = len(x_data) # Número de datos
+    
+    for i in range(n):
+        Li = 1
+        for j in range(n):
+            if j != i:
+                Li *= (x - x_data[j]) / (x_data[i] - x_data[j])
+        P += Li * y_data[i]
+    return sp.expand(P)
